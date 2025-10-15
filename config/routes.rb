a3e37a416a304e7bb2688ade8f74841e63f50dff
tatiana.groiss@users.noreply.github.com <tatiana.groiss@gmail.com>
2025-10-15 20:34:51 +0200

    Routs for Review and Controller Review are created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :show, :new, :create]
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
   # root "posts#index"
 end
